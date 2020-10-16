479030ef3a621869d1eeaf8214d81c8b2ee03c6a
divyapratapsingh4011 <divypratapsingh13@gmail.com>
2020-10-16 14:49:43 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+// import './screens/login_screen.dart';
+import './screens/home_screen.dart';
+import './screens/auth_screen.dart';
+import 'screens/auth_screen.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.dark,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, snap) {
+          if (snap.hasData) return HomeScreen();
+          return AuthScreen();
+        },
+      ),
+      // routes: ,
+    );
+  }
+}
---
+++ lib/models/authentication.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Authentication {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<FirebaseUser> signIn(String mail, String pass) async {
+    final AuthResult authResult =
+        await auth.signInWithEmailAndPassword(email: mail, password: pass);
+    final FirebaseUser user = authResult.user;
+
+    assert(user != null);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    // return user;
+  }
+
+  Future<FirebaseUser> registerUser(String mail, String pass) async {
+    final AuthResult authResult =
+        await auth.createUserWithEmailAndPassword(email: mail, password: pass);
+    final FirebaseUser user = authResult.user;
+
+    assert(user != null);
+    assert(await user.getIdToken() != null);
+    return user;
+  }
+
+  Future<bool> check(String name) async {
+    final QuerySnapshot result = await Firestore.instance
+        .collection('users')
+        .where('name', isEqualTo: name.toLowerCase())
+        .getDocuments();
+    final List<DocumentSnapshot> documents = result.documents;
+    if (documents.length > 0) return false;
+    return true;
+  }
+}
