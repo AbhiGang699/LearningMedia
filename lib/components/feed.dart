cb0e44b70d3d6ee51946ec1723dd76af1fc36807
AbhiGang699 <46847780+AbhiGang699@users.noreply.github.com>
2020-10-27 00:01:24 +0530

    Refreshing of feed implemented !!

--- lib/components/feed.dart
+++ lib/components/feed.dart
@@ -4,46 +4,50 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'file:///C:/Users/Abhi/AndroidStudioProjects/flutter_learning_app/lib/models/article_card.dart';
 
-class Feed extends StatefulWidget{
+class Feed extends StatefulWidget {
   @override
   _FeedState createState() => _FeedState();
 }
 
 class _FeedState extends State<Feed> {
-  bool _isLoading=true;
+  bool _isLoading = true;
   List<DocumentSnapshot> _arti;
+  var _len=0;
 
-  Future<List<DocumentSnapshot>> getArticles () async{
-    try{
+  Future<List<DocumentSnapshot>> getArticles() async {
+    try {
       QuerySnapshot art = await Firestore.instance.collection("articles").getDocuments();
-      _arti= art.documents;
-      print(_arti[0].data);
+      _arti = art.documents;
+      if(_len<_arti.length){
+        _len=_arti.length;
+        setState(() {
+        });
+      }
       return _arti;
-    }
-    catch(e){
+    } catch (e) {
       print("sorry couldn't fetch data");
       return null;
     }
   }
 
   @override
-  Widget build(BuildContext context){
-    getArticles();
-    return FutureBuilder(
-        future: getArticles(),
-        builder: (context,snapshot){
-          if(snapshot.hasData){
-            return ListView.builder(
-              itemBuilder :(context,index){
-                return ArticleCard(_arti[index]);
-              },
-              itemCount: _arti.length,
-            );
-          }
-          else{
-            return Center(child: CircularProgressIndicator());
-          }
-        }
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: getArticles,
+      child: FutureBuilder(
+          future: getArticles(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return ArticleCard(_arti[index]);
+                },
+                itemCount: _arti.length,
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 }
