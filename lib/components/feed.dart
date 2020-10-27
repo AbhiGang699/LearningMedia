39ccf4398b33d40a1c6e7932245fca4b2ee544b9
divyapratapsingh4011 <divypratapsingh13@gmail.com>
2020-10-27 09:33:21 +0530

    article backend

--- lib/components/feed.dart
+++ lib/components/feed.dart
@@ -1,71 +1,95 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Feed extends StatefulWidget {
-=======
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/components/article_card.dart';
 
-class Feed extends StatefulWidget{
->>>>>>> 50474cb132c834acdbf5cc1fb450d8f2c8e6633b
+class Feed extends StatefulWidget {
   @override
   _FeedState createState() => _FeedState();
 }
 
 class _FeedState extends State<Feed> {
-<<<<<<< HEAD
-  @override
-  Widget build(BuildContext context) {
-    Firestore.instance.collection('articles').getDocuments().then((value) {
-      var v = value.documents;
-      for (int i = 0; i < v.length; i++) print(v[i]['tag']);
-    });
-    return Scaffold(
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   child: Icon(Icons.sort),
-      // ),
-      body: null,
-=======
-  bool _isLoading=true;
+  bool _isLoading = true;
   List<DocumentSnapshot> _arti;
 
-  Future<List<DocumentSnapshot>> getArticles () async{
-    try{
-      QuerySnapshot art = await Firestore.instance.collection("articles").getDocuments();
-      _arti= art.documents;
+  Future<List<DocumentSnapshot>> getArticles(BuildContext context) async {
+    try {
+      QuerySnapshot art =
+          await Firestore.instance.collection("articles").getDocuments();
+      _arti = art.documents;
       print(_arti[0].data);
       return _arti;
-    }
-    catch(e){
+    } catch (e) {
       print("sorry couldn't fetch data");
       return null;
     }
   }
 
   @override
-  Widget build(BuildContext context){
-    getArticles();
+  Widget build(BuildContext context) {
+    getArticles(context);
     return FutureBuilder(
-        future: getArticles(),
-        builder: (context,snapshot){
-          if(snapshot.hasData){
-            return ListView.builder(
-              itemBuilder :(context,index){
-                return ArticleCard(_arti[index]);
-              },
-              itemCount: _arti.length,
-            );
-          }
-          else{
-            return Center(child: CircularProgressIndicator());
-          }
-        }
->>>>>>> 50474cb132c834acdbf5cc1fb450d8f2c8e6633b
+      future: getArticles(context),
+      builder: (context, snapshot) =>
+          snapshot.connectionState == ConnectionState.waiting
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : RefreshIndicator(
+                  onRefresh: () => getArticles(context),
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return ArticleCard(_arti[index]);
+                      },
+                      itemCount: _arti.length,
+                    ),
+                  ),
+                ),
     );
   }
 }
+
+// FutureBuilder(
+//         future: _refreshProducts(context),
+//         builder: (ctx, snapshot) =>
+//             snapshot.connectionState == ConnectionState.waiting
+//                 ? Center(
+//                     child: CircularProgressIndicator(),
+//                   )
+//                 : RefreshIndicator(
+//                     onRefresh: () => _refreshProducts(context),
+//                     child: Consumer<Products>(
+//                       builder: (ctx, productsData, _) => Padding(
+//                             padding: EdgeInsets.all(8),
+//                             child: ListView.builder(
+//                               itemCount: productsData.items.length,
+//                               itemBuilder: (_, i) => Column(
+//                                     children: [
+//                                       UserProductItem(
+//                                         productsData.items[i].id,
+//                                         productsData.items[i].title,
+//                                         productsData.items[i].imageUrl,
+//                                       ),
+//                                       Divider(),
+//                                     ],
+//                                   ),
+//                             ),
+//                           ),
+//                     ),
+//                   ),
+//       ),
+
+//       if(snapshot.hasData){
+//             return ListView.builder(
+//               itemBuilder :(context,index){
+//                 return ArticleCard(_arti[index]);
+//               },
+//               itemCount: _arti.length,
+//             );
+//           }
+//           else{
+//             return Center(child: CircularProgressIndicator());
+//           }
